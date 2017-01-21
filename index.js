c742914f54e1fb61cdd22d95863ad4b5f2ca64d3
bharmee <harmeet.ny@live.com>
2017-01-21 18:13:16 -0500

    Added highlighted hover effect on states

--- index.js
+++ index.js
@@ -1,72 +1,67 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiaGJpbmRyYSIsImEiOiJjaXk3ZG82eTAwMDV5MndvNmdqYW5scDVqIn0.he1_ALGQ23OxOhYo2RXAzA';
 var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/light-v9',
-    center: [-77.04, 38.907], // starting position
-    zoom: 3, // starting zoom
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-100.486052, 37.830348],
+    zoom: 3,
     interactive:false
 });
 
-map.on('mousemove', function (e) {
-    document.getElementById('info').innerHTML =
-        // e.point is the x, y coordinates of the mousemove event relative
-        // to the top-left corner of the map
-        JSON.stringify(e.point) + '<br />' +
-            // e.lngLat is the longitude, latitude geographical position of the event
-        JSON.stringify(e.lngLat);
-});
+map.on('load', function () {
+    map.addSource("states", {
+        "type": "geojson",
+        "data": "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_admin_1_states_provinces.geojson"
+    });
 
-// Create a popup, but don't add it to the map yet.
-var popup = new mapboxgl.Popup({
-    closeButton: false,
-    closeOnClick: false
-});
-
-map.on('load', function() {
-
-    // Add a layer showing the places.
     map.addLayer({
-        "id": "places",
-        "type": "symbol",
-        "source": {
-            "type": "geojson",
-            "data": {
-                "type": "FeatureCollection",
-                "features": [{
-                    "type": "Feature",
-                    "properties": {
-                        "description": "Crime Scene",
-                        "icon": "star"
-                    },
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [-77.007481, 38.876516]
-                    }
-                }]
-            }
-        },
-        "layout": {
-            "icon-image": "{icon}-15",
-            "icon-allow-overlap": true
+        "id": "state-fills",
+        "type": "fill",
+        "source": "states",
+        "layout": {},
+        "paint": {
+            "fill-color": "#627BC1",
+            "fill-opacity": 0.5
         }
     });
-});
 
-map.on('mousemove', function(e) {
-    var features = map.queryRenderedFeatures(e.point, { layers: ['places'] });
-    // Change the cursor style as a UI indicator.
-    map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
+    map.addLayer({
+        "id": "state-borders",
+        "type": "line",
+        "source": "states",
+        "layout": {},
+        "paint": {
+            "line-color": "#627BC1",
+            "line-width": 2
+        }
+    });
 
-    if (!features.length) {
-        popup.remove();
-        return;
-    }
+    map.addLayer({
+        "id": "state-fills-hover",
+        "type": "fill",
+        "source": "states",
+        "layout": {},
+        "paint": {
+            "fill-color": "#627BC1",
+            "fill-opacity": 1
+        },
+        "filter": ["==", "name", ""]
+    });
 
-    var feature = features[0];
+    // When the user moves their mouse over the page, we look for features
+    // at the mouse position (e.point) and within the states layer (states-fill).
+    // If a feature is found, then we'll update the filter in the state-fills-hover
+    // layer to only show that state, thus making a hover effect.
+    map.on("mousemove", function(e) {
+        var features = map.queryRenderedFeatures(e.point, { layers: ["state-fills"] });
+        if (features.length) {
+            map.setFilter("state-fills-hover", ["==", "name", features[0].properties.name]);
+        } else {
+            map.setFilter("state-fills-hover", ["==", "name", ""]);
+        }
+    });
 
-    // Populate the popup and set its coordinates
-    // based on the feature found.
-    popup.setLngLat(feature.geometry.coordinates)
-        .setHTML(feature.properties.description)
-        .addTo(map);
+    // Reset the state-fills-hover layer's filter when the mouse leaves the map
+    map.on("mouseout", function() {
+        map.setFilter("state-fills-hover", ["==", "name", ""]);
+    });
 });
